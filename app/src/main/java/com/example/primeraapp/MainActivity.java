68808db393811f4c6cfe46d0079dd40446ec8977
SnFlores <sergio.norambuena.flores@gmail.com>
2021-05-16 20:29:26 -0400

    proyecto final primera app

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton Ball;
+    private TextView respuesta;
+    private String [] respuestaArray={"\"Es cierto\", \"Definitivamente es así\", \"Sin duda\", \"Sí definitivamente\", \"Puede confiar en él\", \"Como yo lo veo, sí\",\"Lo más probable\", \"Perspectiva buena\", \"Sí\", \"Las señales apuntan a sí\", \"Respuesta confusa intente de nuevo\", \"Pregunte de nuevo más tarde\",\"Mejor no decirte ahora\", \"No puedo predecir ahora\", \"Concéntrate y pregunta de nuevo\", \"No cuentes con eso\",\"Mi respuesta es no\", \"Mis fuentes dicen que no\", \"Outlook no es tan bueno\", \"Muy dudoso\""}
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //referencia de la imagen
+        Ball = findViewById(R.id.btn);
+
+        //referencia del texto
+        respuesta = findViewById(R.id.respuestavista);
+
+        //Proceso
+        Ball.setOnClickListener(this);
+        Toast.makeText(MainActivity.this, "Consulta tu suerte", Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    public void onClick(View v){
+        //Creacion metodo switch
+    switch (v.getId()){
+        case R.id.btn:
+
+            int rand = new Random().nextInt(respuestaArray.length);
+            respuesta.setText(respuestaArray[rand]);
+
+            break;
+        }
+    }
+}
